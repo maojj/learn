cc88622708caae91a30d8d2a647b5c86512d431a
maojj <maojiajun86@gmail.com>
2015-03-17 21:47:05 +0800

    ADD:

--- mail/parser.py
+++ mail/parser.py
@@ -1,11 +1,14 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 #!/usr/bin/python
 
 import csv
 
-with open('name.csv') as csvfile:
+with open('bj5_IpResolved.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
+	index = 0
 	for row in reader:
-		print(row['first'], row['second'])
+		index += 1
+		if index > 100:
+			break
+		print(row['Sender'], row['Recipient(s)'],row['Timestamp'], row['Subject'],row['SenderIP'], row['SenderHostName'],row['Mail'])
 
-		
